bfe3efd2e8f9e141873088bb91e13a1ff665cca2
dze <dzzze@protonmail.com>
2020-06-02 13:48:56 +0900

    Compartmentalize add_custom_test

--- CMake/add_custom_test.cmake
+++ CMake/add_custom_test.cmake
@@ -1,51 +1,13 @@
-function(add_custom_test)
-    set(options "")
-    set(one_value_args NAME)
-    set(multi_value_args DEPENDS CUSTOM_TARGET_DEPENDS)
-    cmake_parse_arguments(
-        ARG
-        "${options}"
-        "${one_value_args}"
-        "${multi_value_args}"
-        ${ARGN})
+include_guard(GLOBAL)
 
-    foreach (dep ${ARG_DEPENDS})
-        list(APPEND deps ${dep})
-    endforeach ()
+include(FetchContent)
 
-    foreach (dep ${ARG_CUSTOM_TARGET_DEPENDS})
-        list(APPEND deps ${dep})
-        list(APPEND deps "$<TARGET_PROPERTY:${dep},dependent_files>")
-    endforeach ()
+FetchContent_Populate(
+    add_custom_test_proj
+    GIT_REPOSITORY https://github.com/dzzze/add_custom_test
+    GIT_TAG 52b73546033586bd5b2495d45a89799274dfe78f
+    GIT_SHALLOW true
+    PREFIX "${CMAKE_CURRENT_BINARY_DIR}/add_custom_test"
+    SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/add_custom_test/source")
 
-    if (ARG_NAME STREQUAL "")
-        message(FATAL_ERROR "add_custom_test called without argument for name")
-    endif ()
-
-    set(name ${ARG_NAME})
-
-    add_test(NAME ${name} ${ARG_UNPARSED_ARGUMENTS})
-
-    set(stamp_file "${CMAKE_CURRENT_BINARY_DIR}/${name}_stamp")
-
-    add_custom_command(
-        OUTPUT ${stamp_file}
-        COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --progress -R '^${name}$$'
-        COMMAND ${CMAKE_COMMAND} -E touch ${stamp_file}
-        USES_TERMINAL
-        COMMENT "Running test ${name}"
-        DEPENDS ${deps})
-
-    add_custom_target(
-        ${PROJECT_NAME}-run-custom-test-${name}
-        DEPENDS ${stamp_file})
-
-    set_target_properties(
-        ${PROJECT_NAME}-run-custom-test-${name}
-        PROPERTIES
-        dependent_files ${stamp_file})
-
-    if (TARGET ${PROJECT_NAME}-run-custom-tests)
-        add_dependencies(${PROJECT_NAME}-run-custom-tests ${PROJECT_NAME}-run-custom-test-${name})
-    endif ()
-endfunction()
+include(${add_custom_test_proj_SOURCE_DIR}/add_custom_test.cmake)
